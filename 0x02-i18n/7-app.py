338d153b139f952bdfc5e733a3795a2f561ee1b0
Mpho Kekana <111887473+mphokekana@users.noreply.github.com>
2023-08-01 19:06:03 +0000

    Task

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""A Basic Flask app with internationalization support.
+"""
+import pytz
+from flask_babel import Babel
+from typing import Union, Dict
+from flask import Flask, render_template, request, g
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
